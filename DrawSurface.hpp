77fd02b0d8480cde0ef50af2765e4180107d82fb
Antonio Setya <antonioarmyn@gmail.com>
2019-09-05 12:42:01 +0700

    Showing file dialog on Load File click & getting file URL

--- DrawSurface.hpp
+++ DrawSurface.hpp
@@ -1,3 +1,4 @@
+#include <QPainter>
 #include <QPaintEvent>
 #include <QWidget>
 #include "datastruct/Image.hpp"
--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
 #include <QApplication>
-#include <QLabel>
-#include <QMainWindow>
-#include <QMenuBar>
-#include <QPainter>
-#include <QPaintEvent>
-#include <QRegion>
-#include "DrawSurface.hpp"
+#include "MainWindow.hpp"
 #include "file_handling/loader/ImageLoaderFactory.hpp"
+
 
 int main (int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QMainWindow rootWindow;
+    MainWindow rootWindow;
     rootWindow.setWindowTitle("SotoShop");
-    rootWindow.setFixedSize(800,600);
-
-    QMenu * fileMenu = rootWindow.menuBar()->addMenu("File");
-    QAction * loadAction = fileMenu->addAction("Load");
-    QAction * saveAction = fileMenu->addAction("Save");
-
-    DrawSurface drawspace(&rootWindow);
-    rootWindow.setCentralWidget(&drawspace);
-    QRegion tempRegion;
-    drawspace.paintEvent(new QPaintEvent(tempRegion));
+    rootWindow.setFixedSize(800,600); 
 
     //ImageLoader * imageLoader = ImageLoaderFactory::getImageLoader("test.ppm");
 
