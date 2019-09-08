5e27895ad1a5594f3b5d93dd4d7051246c52fc01
teresachn <33697410+teresachn@users.noreply.github.com>
2019-09-08 15:04:48 +0700

    Add initial image operator class

---
+++ operation/ImageOperator.cpp
@@ -0,0 +1,6 @@
+#include "ImageOperator.h"
+
+ImageOperator::ImageOperator()
+{
+
+}
---
+++ operation/ImageOperator.h
@@ -0,0 +1,14 @@
+#ifndef IMAGEOPERATOR_H
+#define IMAGEOPERATOR_H
+
+#include "../datastruct/Image.hpp"
+
+class ImageOperator
+{
+public:
+    ImageOperator();
+
+
+};
+
+#endif // IMAGEOPERATOR_H
