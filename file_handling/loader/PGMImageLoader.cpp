82efc77fe123b287b4493f0b06cb8dbed19b2803
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-09-15 20:49:31 +0700

    Finished Binary PGM Loader

--- file_handling/loader/PGMImageLoader.cpp
+++ file_handling/loader/PGMImageLoader.cpp
@@ -5,6 +5,7 @@
 
 #define ASCII_PGM "P2"
 #define BINARY_PGM "P5"
+#define MAX_COLOR 255
 
 Image * loadASCII(std::string filename);
 Image * loadBinary(std::string filename);
@@ -37,13 +38,14 @@ Image * loadASCII(std::string filename) {
     std::string fileType;
     std::string dimensions;
     std::string level;
-    int height, width, grayLevel;
+    int height = 0, width = 0, grayLevel = 0;
     std::ifstream fileread(filename);
     if (fileread.good()) {
         std::getline(fileread, fileType);
         while (fileType[0] == '#') {
             std::getline(fileread, fileType);
         }
+        fileread.close();
         if (fileType == ASCII_PGM) {
             std::getline(fileread, dimensions);
             while (dimensions[0] == '#') {
@@ -56,8 +58,7 @@ Image * loadASCII(std::string filename) {
             width = std::stoi(dimensions.substr(0, dimensions.find(' ')));
             height = std::stoi(dimensions.substr(level.find(' ') + 1, dimensions.length()));
             level = std::stoi(level);
-            std::cout << "Image dimensions: ", width, "x", height, "\n";
-            std::cout << "Gray level      : ", level;
+            return new Image(width, height);
         } else {
             throw std::runtime_error("Not PGM File");
         }
@@ -71,6 +72,7 @@ Image * loadBinary(std::string filename) {
     std::string dimensions;
     std::string level;
     int height, width, grayLevel;
+    unsigned char *buff;
     std::ifstream fileread(filename);
     if (fileread.good()) {
         std::getline(fileread, fileType);
@@ -87,10 +89,25 @@ Image * loadBinary(std::string filename) {
                 std::getline(fileread, level);
             }
             width = std::stoi(dimensions.substr(0, dimensions.find(' ')));
-            height = std::stoi(dimensions.substr(level.find(' ') + 1, dimensions.length()));
-            level = std::stoi(level);
-            std::cout << "Image dimensions: ", width, "x", height, "\n";
-            std::cout << "Gray level      : ", level;
+            height = std::stoi(dimensions.substr(dimensions.find(' ') + 1, dimensions.length()));
+            grayLevel = std::stoi(level);
+            Pixel **data = new Pixel*[height];
+            for (int j = 0; j < height; j++) {
+                data[j] = new Pixel[width];
+            }
+            int imageSize = height*width;
+            buff = new unsigned char [imageSize];
+            fileread.read((char*) buff, sizeof(char)*height*width);
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    int value = ((int) buff[i*width + j] * MAX_COLOR / grayLevel);
+                    data[i][j].setRed(value);
+                    data[i][j].setGreen(value);
+                    data[i][j].setBlue(value);
+                }
+            }
+            fileread.close();
+            return new Image(width, height, data, "pgm");
         } else {
             throw std::runtime_error("Not PGM File");
         }
