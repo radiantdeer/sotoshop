505eae7cbbe105c0b2bc6f69fe74084ddf700c54
Antonio Setya <antonioarmyn@gmail.com>
2019-10-02 19:48:52 +0700

    Logging library
    Using "spdlog"

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <QApplication>
 #include "frontend/MainWindow.hpp"
+#include "spdlog/spdlog.h"
+
 
 int main (int argc, char *argv[]) {
+    spdlog::info("Initializing SotoShop...");
+    spdlog::info("Log level set to {}", "debug");
+    spdlog::set_level(spdlog::level::debug);
     QApplication app(argc, argv);
     MainWindow rootWindow;
     rootWindow.setWindowTitle("SotoShop");
-    rootWindow.setFixedSize(800,600); 
+    rootWindow.setFixedSize(800,600);
     rootWindow.show();
 
     /* STUB IMAGE DATA */
     Image * image = new Image(500, 400);
-    rootWindow.setActiveImage(image); 
+    rootWindow.setActiveImage(image);
 
     return app.exec();
 }
